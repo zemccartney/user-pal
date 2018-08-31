17027390b8096455b439cd7c34fab808c4c30418
Zack McCartney <zack@bigroomstudios.com>
2018-08-31 13:50:10 -0400

    Update deps and retest

---
+++ server/manifest.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('confidence');
+
+// Pull .env into process.env
+if (process.env.NODE_ENV === 'test'){
+    Dotenv.config({ path: `${__dirname}/.env-test` });
+}
+else {
+    Dotenv.config({ path: `${__dirname}/.env` });
+}
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: '0.0.0.0',
+        port: process.env.PORT || 3000,
+        debug: {
+            $filter: 'NODE_ENV',
+            development: {
+                log: ['error', 'implementation', 'internal'],
+                request: ['error', 'implementation', 'internal']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {
+                    jwtKey: process.env.JWT_SECRET
+                },
+                routes: {
+                    prefix: process.env.API_PREFIX
+                }
+            },
+            {
+                plugin: './plugins/swagger'
+            },
+            {
+                plugin: 'schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'sqlite3',
+                            useNullAsDefault: true,         // Suggested for sqlite3
+                            pool: {
+                                idleTimeoutMillis: Infinity // Handles knex v0.12/0.13 misconfiguration when using sqlite3 (tgriesser/knex#1701)
+                            },
+                            connection: {
+                                filename: ':memory:'
+                            }
+                        }
+                    },
+                    development: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'pg',
+                            useNullAsDefault: true,
+                            connection: {
+                                host: process.env.DB_HOST,
+                                user: process.env.DB_USER,
+                                password: process.env.DB_PASSWORD,
+                                database: process.env.DB_NAME
+                            }
+                        }
+                    },
+                    test: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'pg',
+                            useNullAsDefault: true,
+                            connection: {
+                                host: process.env.DB_HOST,
+                                user: process.env.DB_USER,
+                                password: process.env.DB_PASSWORD,
+                                database: process.env.DB_NAME
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
+            }
+        ]
+    }
+});
